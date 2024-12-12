043eea6e3338bc3f16c8d7f72b250db231196dfe
AleksandrKompanetc <moneymacker7@gmail.com>
2024-12-12 22:39:19 +0200

    adding app with fetch request

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
@@ -8,20 +8,44 @@ function App() {
 
   const fetchUsers = async () => {
     try {
-
+      const response = await fetch('https://jsonplaceholder.typicode.com/users')
+      if (!response.ok) {
+        throw new Error('Loading data error!')
+      }
+      const data = await response.json()
+      setUsers(data)
     } catch(err) {
-
+      setError(err.message)
     } finally {
-
+      setLoading(false)
     }
   }
+
+  useEffect(() => {
+    fetchUsers()
+  }, [])
 
   return (
     <div style={{padding: '20px', fontFamily: 'Arial, sans-serif'}}>
       <h1>User List</h1>
       {loading && <p>Loading data...</p>}
       {error && <p style={{color: 'red'}}>Error: {error}</p>}
-
+      {!loading && !error && (
+        <ul style={{listStyle: 'none', padding: 0}}>
+          {users.map((user) => (
+            <li key={user.id} style={{
+              padding: '10px',
+              margin: '10px 0',
+              border: '1px solid #ccc',
+              borderRadius: '5px',
+            }}>
+              <strong>{user.name}</strong> <br />
+              <em>Email:</em>{user.email} <br />
+              <em>Phone:</em>{user.phone}
+            </li>
+          ))}
+        </ul>
+      )}
 
     </div>
   )
